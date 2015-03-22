c7a06e8a1e49ca0d211b2b51904f66c4b9d7429c
Dmitry Nikitin <nikitin.da.90@gmail.com>
2015-03-22 14:23:52 +0300

    Enable constant names checkstyle property

--- storio/src/androidTest/java/com/pushtorefresh/storio/db/integration_test/impl/TestFactory.java
+++ storio/src/androidTest/java/com/pushtorefresh/storio/db/integration_test/impl/TestFactory.java
@@ -11,10 +11,10 @@ public class TestFactory {
     private TestFactory() {
     }
 
-    private static final AtomicInteger counter = new AtomicInteger(0);
+    private static final AtomicInteger COUNTER = new AtomicInteger(0);
 
     @NonNull public static User newUser() {
-        return new User(null, "user" + counter.incrementAndGet() + "@example.com");
+        return new User(null, "user" + COUNTER.incrementAndGet() + "@example.com");
     }
 
     @NonNull public static List<User> newUsers(int quantity) {
