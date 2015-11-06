149c2dcf2d386c55f7023bf832b50e1386cf13ad
Artem Zinnatullin <artem-zin@yandex-team.ru>
2015-11-06 17:13:17 +0300

    StorIOContentReslver fix for observing changes of Uris on Android API < 16

---
+++ storio-test-common/src/test/java/com/pushtorefresh/storio/test/UtilsTest.java
@@ -0,0 +1,29 @@
+package com.pushtorefresh.storio.test;
+
+import com.pushtorefresh.private_constructor_checker.PrivateConstructorChecker;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilsTest {
+
+    @Test
+    public void constructorShouldBePrivateAndThrowException() {
+        PrivateConstructorChecker
+                .forClass(Utils.class)
+                .expectedTypeOfException(IllegalStateException.class)
+                .expectedExceptionMessage("No instances please!")
+                .check();
+    }
+
+    @Test
+    public void maxSdkVersionShouldBeAtLeast23() {
+        assertThat(Utils.MAX_SDK_VERSION).isGreaterThanOrEqualTo(23);
+    }
+
+    @Test
+    public void minSdkVersionShouldBe14() {
+        assertThat(Utils.MIN_SDK_VERSION).isEqualTo(14);
+    }
+}
