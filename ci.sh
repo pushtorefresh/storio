83017a48591c10fd651be5b4c6597c2ca569e3ba
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2015-11-17 18:39:17 +0300

    Android Gradle Plugin 1.5.0

--- ci.sh
+++ ci.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Please run it from root project directory
 ./gradlew clean build jacocoReport checkstyle -PdisablePreDex
+
+# TODO REMOVE
+cat storio-common/build/outputs/lint-results.xml
