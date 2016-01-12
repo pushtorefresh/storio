2838d71a78a32a31e96267d06f0e3d88ecac2e93
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2016-01-12 23:41:18 +0300

    Add asRxObservable(), deprecate createObservable()

--- storio-content-resolver/src/main/java/com/pushtorefresh/storio/contentresolver/operations/get/PreparedGetListOfObjects.java
+++ storio-content-resolver/src/main/java/com/pushtorefresh/storio/contentresolver/operations/get/PreparedGetListOfObjects.java
@@ -123,12 +123,39 @@ public final class PreparedGetListOfObjects<T> extends PreparedGet<List<T>> {
      * @return non-null {@link Observable} which will emit non-null, immutable
      * {@link List} with mapped results and will be subscribed to changes of {@link #query} Uri,
      * list can be empty.
+     * @deprecated (will be removed in 2.0) please use {@link #asRxObservable()}.
      */
     @NonNull
     @CheckResult
     @Override
     public Observable<List<T>> createObservable() {
-        throwExceptionIfRxJavaIsNotAvailable("createObservable()");
+        return asRxObservable();
+    }
+
+    /**
+     * Creates "Hot" {@link Observable} which will be subscribed to changes of {@link #query} Uri
+     * and will emit result each time change occurs.
+     * <p>
+     * First result will be emitted immediately after subscription,
+     * other emissions will occur only if changes of {@link #query} Uri will occur.
+     * <p>
+     * <dl>
+     * <dt><b>Scheduler:</b></dt>
+     * <dd>Operates on {@link Schedulers#io()}.</dd>
+     * </dl>
+     * <p>
+     * Please don't forget to unsubscribe from this {@link Observable}
+     * because it's "Hot" and endless.
+     *
+     * @return non-null {@link Observable} which will emit non-null, immutable
+     * {@link List} with mapped results and will be subscribed to changes of {@link #query} Uri,
+     * list can be empty.
+     */
+    @NonNull
+    @CheckResult
+    @Override
+    public Observable<List<T>> asRxObservable() {
+        throwExceptionIfRxJavaIsNotAvailable("asRxObservable()");
 
         return storIOContentResolver
                 .observeChangesOfUri(query.uri()) // each change triggers executeAsBlocking
