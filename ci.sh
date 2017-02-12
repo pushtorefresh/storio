1f81c4beb70f1b454fda70a4af486f3f84c54583
Ilya Zorin <geraltencore@gmail.com>
2017-02-12 23:47:39 +0200

    CI fix

--- ci.sh
+++ ci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Please run it from root project directory
-./gradlew clean build checkstyle -PdisablePreDex
+./gradlew clean build checkstyle -PdisablePreDex -x :storio-sqlite-annotations-processor-test:test && ./gradlew :storio-sqlite-annotations-processor-test:testDebugUnitTest
