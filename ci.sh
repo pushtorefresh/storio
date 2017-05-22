131ecb42b5599bd7993b1ec786a6b0ee0b65f048
Artem Zinnatullin :slowpoke <artem.zinnatullin@gmail.com>
2017-05-22 18:22:01 +0300

    Add set -e.

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -e
+
 # Please run it from root project directory
 # For some reason test for annotation processor are failing on a regular CI setup.
 # So we had to exclude test task for it from the main build process and execute it as a separate command.
 ./gradlew clean build checkstyle -PdisablePreDex -x :storio-sqlite-annotations-processor-test:test -x :storio-content-resolver-annotations-processor-test:test && ./gradlew :storio-sqlite-annotations-processor-test:testDebugUnitTest && ./gradlew :storio-content-resolver-annotations-processor-test:testDebugUnitTest
-
-cat storio-basic-sample-app/build/reports/lint-results.xml
