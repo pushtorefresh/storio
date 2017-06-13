13a5386ffebfa849763fa5fdd26e0a4cd9e0c284
Ilya Zorin <geraltencore@gmail.com>
2017-06-13 18:15:35 +0300

    #797 Proper message in case creator parameters do not match columns (#798)
    * #797 Proper message in case creator parameters do not match columns
    
    * #797 Improve params names

--- storio-content-resolver-annotations-processor-test/src/test/resources/CreatorWithNotMatchingArguments.java
+++ storio-content-resolver-annotations-processor-test/src/test/resources/CreatorWithNotMatchingArguments.java
@@ -1,15 +1,17 @@
 package com.pushtorefresh.storio.contentresolver.annotations;
 
 @StorIOContentResolverType(uri = "content://uri")
-public class CreatorWithWrongNumberOfArguments {
+public class CreatorWithNotMatchingArguments {
+
+    private long id;
 
     @StorIOContentResolverColumn(name = "id", key = true)
     long id() {
-        return 0;
+        return id;
     }
 
     @StorIOContentResolverCreator
-    static CreatorWithWrongNumberOfArguments creator(long id, String some) {
-        return new CreatorWithWrongNumberOfArguments();
+    public CreatorWithNotMatchingArguments(long notMatchingName) {
+        this.id = notMatchingName;
     }
 }
