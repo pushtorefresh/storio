b249b7a68ac3b55fbb012e8ff9fb34a2c4dd694f
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2017-10-15 22:50:26 -0700

    Use environment variable to detect publishing state.

--- ci.sh
+++ ci.sh
@@ -7,16 +7,14 @@ PROJECT_DIR="$DIR"
 
 pushd "$PROJECT_DIR"
 
-# Pass "publish=true" as first argument to initiate release process.
-SHOULD_PUBLISH_RELEASE="$1"
-
 # For some reason test for annotation processor are failing on a regular CI setup.
 # So we had to exclude test task for it from the main build process and execute it as a separate command.
 ./gradlew clean build checkstyle -PdisablePreDex -x :storio-sqlite-annotations-processor-test:test -x :storio-content-resolver-annotations-processor-test:test
 ./gradlew :storio-sqlite-annotations-processor-test:testDebugUnitTest
 ./gradlew :storio-content-resolver-annotations-processor-test:testDebugUnitTest
 
-if [ "$SHOULD_PUBLISH_RELEASE" == "publish=true" ]; then
+# Export "PUBLISH_RELEASE=true" to initiate release process.
+if [ "$PUBLISH_RELEASE" == "true" ]; then
     echo "Launching release publishing process..."
 
     if [ -z "$GPG_SECRET_KEYS" ]; then
