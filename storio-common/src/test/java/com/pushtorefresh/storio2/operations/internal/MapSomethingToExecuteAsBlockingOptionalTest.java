12cd8bcc97c2d3916829f60dc3ab821ff94798af
Dmitrii Nikitin <nikitin.da.90@gmail.com>
2017-11-18 17:19:24 +0300

    Add tests for common subscribe utlis

---
+++ storio-common/src/test/java/com/pushtorefresh/storio2/operations/internal/MapSomethingToExecuteAsBlockingOptionalTest.java
@@ -0,0 +1,40 @@
+package com.pushtorefresh.storio2.operations.internal;
+
+import com.pushtorefresh.storio2.Optional;
+import com.pushtorefresh.storio2.operations.PreparedOperation;
+
+import org.junit.Test;
+
+import io.reactivex.Flowable;
+import io.reactivex.subscribers.TestSubscriber;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class MapSomethingToExecuteAsBlockingOptionalTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void verifyBehavior() {
+        final PreparedOperation<String, Optional<String>, Object> preparedOperation = mock(PreparedOperation.class);
+
+        final String expectedMapResult = "expected_string";
+
+        when(preparedOperation.executeAsBlocking())
+                .thenReturn(expectedMapResult);
+
+        TestSubscriber<Optional<String>> testSubscriber = new TestSubscriber<Optional<String>>();
+
+        Flowable
+                .just(1)
+                .map(new MapSomethingToExecuteAsBlockingOptional<Integer, String, Object>(preparedOperation))
+                .subscribe(testSubscriber);
+
+        verify(preparedOperation).executeAsBlocking();
+
+        testSubscriber.assertValue(Optional.of(expectedMapResult));
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertComplete();
+    }
+}
